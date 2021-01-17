0aa149e4e3b68d616cd2a6c0172e337c67c4fbc8
Erico Andre <ericoandresilva@gmail.com>
2021-01-17 13:18:06 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            itens:1
+        },
+        600:{
+            itens:3
+        },
+        1000:{
+            itens:5
+        }
+    }
+})
